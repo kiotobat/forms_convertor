5d3168e7442a8abd959704a45e5ae37a601f2a1f
kiotobat <lambar926@gmail.com>
2025-08-12 16:43:14 +0300

    redit vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_forms-converter/',
+  base: '/forms_converter/',
   server: {
     open: true,
   },
